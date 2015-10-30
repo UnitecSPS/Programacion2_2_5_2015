e9ad5d2dc642c9912e81367a09ab079d67ffbad1
Carlos Gochez <cgotcha@gmail.com>
2015-10-30 08:36:01 -0600

    Estructura de TG2

---
+++ src/herencia/tg2/PlanBlackberry.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package herencia.tg2;
+
+/**
+ *
+ * @author mac
+ */
+public class PlanBlackberry {
+    
+    public void addPin(String pin){
+        
+    }
+    
+}
---
+++ src/herencia/tg2/Tigo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package herencia.tg2;
+
+/**
+ *
+ * @author mac
+ */
+public class Tigo {
+    
+    public void addPlan(int numtel,String nombre,  String extra, String tipo){
+        
+    }
+    
+    public boolean búsqueda(int numtel, String datoExtra, String tipo){
+        return false;
+    }
+    
+    public double pagoPlan(int numTel, int mins,int msgs){
+        return 0;
+    }
+   
+    public void addAmigo(int numTel, String pin){
+        
+    }
+    
+    public void list(){
+        
+    }
+    
+}
